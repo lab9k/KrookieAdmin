511c83ce0f806b6d20e0a595f8d0e5f66167930f
WannesV <wannes.vereecken@hotmail.be>
2017-09-21 13:02:10 +0200

    Added regex for beacon

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,6 +1,7 @@
 class Shelf < ApplicationRecord
-    validates :mainbeacon, presence: true
-    validates :beacon1, presence: true
-    validates :beacon2, presence: true
-    validates :beacon3, presence: true
+    regex = /\A[a-fA-F0-9]{8}\-[a-fA-F0-9]{4}\-[a-fA-F0-9]{4}\-[a-fA-F0-9]{4}\-[a-fA-F0-9]{12}\z/
+    validates :mainbeacon, presence: true, :format => {:with => regex}
+    validates :beacon1, presence: true, :format => {:with => regex}
+    validates :beacon2, presence: true, :format => {:with => regex}
+    validates :beacon3, presence: true, :format => {:with => regex}
 end
